43915a2ebc803c6583a199ccb374107a754c7dab
Will Nayes <wnayes@gmail.com>
2018-05-15 22:19:30 -0500

    Conform closer to ABI. Store caller saved registers in the local data area. Pad final stack frame to 8 bytes.

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-
-const calleeSaved = [
+/** MIPS registers. */
+const regs = [
+  "at",
+  "v0",
+  "v1",
+  "a0",
+  "a1",
+  "a2",
+  "a3",
+  "t0",
+  "t1",
+  "t2",
+  "t3",
+  "t4",
+  "t5",
+  "t6",
+  "t7",
+  "t8",
+  "t9",
+  "s0",
+  "s1",
+  "s2",
+  "s3",
+  "s4",
+  "s5",
+  "s6",
+  "s7",
+  // "k0",
+  // "k1",
+  "gp",
+  "sp",
+  "fp",
   "ra",
+];
+
+/** Registers that are saved by the callee by convention. */
+const calleeSaved = [
   "s0",
   "s1",
   "s2",
@@ -12,44 +46,39 @@ const calleeSaved = [
   "gp",
   "sp",
   "fp",
+  "ra",
+];
+
+/** Registers that are saved by the caller by convention. */
+const callerSaved = [
+  "at",
+  "v0",
+  "v1",
+  "a0",
+  "a1",
+  "a2",
+  "a3",
+  "t0",
+  "t1",
+  "t2",
+  "t3",
+  "t4",
+  "t5",
+  "t6",
+  "t7",
+  "t8",
+  "t9",
 ];
 
 const state = {
   argCount: 0,
   localDataWords: 0,
-  regs: {
-    at: false,
-    v0: false,
-    v1: false,
-    a0: false,
-    a1: false,
-    a2: false,
-    a3: false,
-    t0: false,
-    t1: false,
-    t2: false,
-    t3: false,
-    t4: false,
-    t5: false,
-    t6: false,
-    t7: false,
-    t8: false,
-    t9: false,
-    s0: false,
-    s1: false,
-    s2: false,
-    s3: false,
-    s4: false,
-    s5: false,
-    s6: false,
-    s7: false,
-    // k0: false,
-    // k1: false,
-    gp: false,
-    sp: false,
-    fp: false,
-    ra: true,
-  }
+  usedRegisters: {},
+}
+
+// Initialize all registers as unused, except RA.
+for (let reg of regs) {
+  state.usedRegisters[reg] = reg === "ra" ? true : false;
 }
 
 new Vue({
@@ -60,7 +89,16 @@ new Vue({
     savedRegisterCount: function() {
       let count = 0;
       for (let reg of calleeSaved) {
-        if (this.regs[reg]) {
+        if (this.usedRegisters[reg]) {
+          count++;
+        }
+      }
+      return count;
+    },
+    savedTempRegisterCount: function() {
+      let count = 0;
+      for (let reg of callerSaved) {
+        if (this.usedRegisters[reg]) {
           count++;
         }
       }
@@ -70,88 +108,92 @@ new Vue({
 });
 
 function updateAssembly() {
-  const argumentSectionSize = this.argCount * 4;
+  const argCount = Math.max(0, this.argCount);
+  const argumentSectionSize = argCount * 4;
   const savedRegSectionSize = this.savedRegisterCount * 4;
-  const localDataSectionSize = this.localDataWords * 4;
+  const savedTempRegSectionSize = this.savedTempRegisterCount * 4;
+  const localDataWords = Math.max(0, this.localDataWords);
+  const localDataSectionSize = localDataWords * 4;
 
   const hasSomeLowerSections = !!argumentSectionSize || !!savedRegSectionSize;
   const lowerSectionsDivBy8 = !!((argumentSectionSize + savedRegSectionSize) % 8);
-  const padSize = (hasSomeLowerSections && lowerSectionsDivBy8) ? 4 : 0;
+  const lowerPadSize = (hasSomeLowerSections && lowerSectionsDivBy8) ? 4 : 0;
 
-  const stackSize = argumentSectionSize + savedRegSectionSize + padSize + localDataSectionSize;
+  const stackSize = argumentSectionSize + savedRegSectionSize + lowerPadSize + savedTempRegSectionSize + localDataSectionSize;
+
+  const upperPadSize = (stackSize % 8) ? 4 : 0;
 
   let asm = "";
 
   if (stackSize > 0) {
-    const padText = padSize ? " ; Including 4 byte pad" : "";
+    const totalPad = lowerPadSize + upperPadSize;
+    const padText = totalPad ? ` ; Including ${totalPad} byte pad` : "";
     asm += `ADDIU SP SP -${stackSize}${padText}\n`;
   }
 
-  const usingRA = this.regs["ra"];
-  const raStackOffset = (argumentSectionSize + savedRegSectionSize - 4);
-  if (usingRA)
-    asm += `SW RA ${raStackOffset}(SP)\n`
-
-  if (this.regs["a0"] || this.regs["a1"] || this.regs["a2"] || this.regs["a3"]) {
-    let wroteComment = false;
-    for (let i = 0; i < Math.min(4, this.argCount); i++) {
-      if (this.regs[`a${i}`]) {
-        if (!wroteComment) {
-          asm += "\n; Save off argument registers:\n";
-          wroteComment = true;
-        }
-        asm += `SW A${i} ${i * 4}(SP)\n`;
-      }
-    }
+  const calleeSavedTopOffset = argumentSectionSize + savedRegSectionSize - 4;
+  if (savedRegSectionSize) {
+    asm += generateSavedRegisterCode(this.usedRegisters, "SW", calleeSavedTopOffset, calleeSaved);
+    asm += "\n";
   }
 
-  if (this.argCount > 4) {
-    asm += "\n; Access additional arguments:\n";
+  const beginCodeMessage = "; Begin code\n\n";
+  asm += beginCodeMessage;
+
+  const callerSavedTopOffset = argumentSectionSize + savedRegSectionSize + lowerPadSize + savedTempRegSectionSize - 4;
+  if (savedTempRegSectionSize) {
+    asm += generateSavedRegisterCode(this.usedRegisters, "SW", callerSavedTopOffset, callerSaved);
+    asm += "\n";
+  }
+
+  if (argCount > 4) {
+    asm += "; Access additional arguments:\n";
     asm += `LW reg 16(SP) ; arg4\n`;
 
-    if (this.argCount > 6) {
+    if (argCount > 6) {
       asm += "...\n";
-      asm += `LW reg ${(this.argCount - 1) * 4}(SP) ; arg${this.argCount - 1}\n`;
+      asm += `LW reg ${(argCount - 1) * 4}(SP) ; arg${argCount - 1}\n`;
     }
-    else if (this.argCount === 6) {
+    else if (argCount === 6) {
       asm += `LW reg 20(SP) ; arg5\n`;
     }
+    asm += "\n";
   }
 
-  if ((!usingRA && this.savedRegisterCount) || (usingRA && this.savedRegisterCount > 1)) {
-    asm += "\n; Save registers:\n";
-    let currentReg = 0;
-    for (let i = 1; i < calleeSaved.length; i++) {
-      const reg = calleeSaved[i];
-      if (this.regs[reg]) {
-        const spOffset = argumentSectionSize + (currentReg * 4);
-        asm += `SW ${reg.toUpperCase()} ${spOffset}(SP)\n`;
-        currentReg++;
-      }
-    }
-  }
+  if (localDataWords) {
+    asm += "; Access local data:\n";
 
-  if (this.localDataWords) {
-    asm += "\n; Access local data:\n";
-
-    let localStackOffset = argumentSectionSize + savedRegSectionSize + padSize;
+    let localStackOffset = argumentSectionSize + savedRegSectionSize + lowerPadSize + savedTempRegSectionSize;
 
     asm += `LW reg ${localStackOffset}(SP) ; local0\n`;
 
-    if (this.localDataWords > 2) {
+    if (localDataWords > 2) {
       asm += "...\n";
-      const lastLocalOffset = localStackOffset + ((this.localDataWords - 1) * 4);
-      asm += `LW reg ${lastLocalOffset}(SP) ; local${this.localDataWords - 1}\n`;
+      const lastLocalOffset = localStackOffset + ((localDataWords - 1) * 4);
+      asm += `LW reg ${lastLocalOffset}(SP) ; local${localDataWords - 1}\n`;
     }
-    else if (this.localDataWords === 2) {
+    else if (localDataWords === 2) {
       asm += `LW reg ${localStackOffset + 4}(SP) ; local1\n`;
     }
+    asm += "\n";
   }
 
-  if (usingRA)
-    asm += `\nLW RA ${raStackOffset}(SP)\n`;
-  else
+  if (savedTempRegSectionSize) {
+    asm += generateSavedRegisterCode(this.usedRegisters, "LW", callerSavedTopOffset, callerSaved);
     asm += "\n";
+  }
+
+  if (asm.endsWith(beginCodeMessage)) {
+    asm = asm.slice(0, -(beginCodeMessage.length));
+    asm += "; Your code\n\n";
+  }
+  else {
+    asm += "; End code\n\n";
+  }
+
+  if (savedRegSectionSize) {
+    asm += generateSavedRegisterCode(this.usedRegisters, "LW", calleeSavedTopOffset, calleeSaved);
+  }
 
   asm += "JR RA\n";
 
@@ -161,4 +203,17 @@ function updateAssembly() {
     asm += "NOP";
 
   return asm.trim();
+}
+
+function generateSavedRegisterCode(usedRegisters, op, maxOffset, regFilter) {
+  let asm = "";
+  let currentOffset = maxOffset;
+  for (let i = regs.length - 1; i >= 0; i--) {
+    const reg = regs[i];
+    if (usedRegisters[reg] && (!regFilter || regFilter.indexOf(reg) >= 0)) {
+      asm += `${op} ${reg.toUpperCase()} ${currentOffset}(SP)\n`;
+      currentOffset -= 4;
+    }
+  }
+  return asm;
 }
